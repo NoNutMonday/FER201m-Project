f1d994a139ef251c0c839fc35bb059497b9bff14
NoNutMonday <trinhphong27042003@gmail.com>
2023-06-12 21:51:54 +0700

    add project

---
+++ FER201m-Project/src/App.js
@@ -0,0 +1,15 @@
+import './App.css'
+import LoadDataToTable from './components/3_loadDataToTable';
+// import GetValueFormInput from "./components/1_GetValue+HandleEvent";
+// import ChangeText from './components/2_userState';
+
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
