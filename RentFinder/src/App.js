93e07eef2e0e0e122badf7f1b76a193a600e7371
NoNutMonday <trinhphong27042003@gmail.com>
2023-06-13 09:26:59 +0700

    Project name: RentFinder

---
+++ RentFinder/src/App.js
@@ -0,0 +1,15 @@
+import './App.css'
+import NavBar from './components/NavBar';
+// import GetValueFormInput from "./components/1_GetValue+HandleEvent";
+// import ChangeText from './components/2_userState';
+
+
+function App() {
+  return (
+    <div className="App">
+        <NavBar/>
+    </div>
+  );
+}
+
+export default App;
