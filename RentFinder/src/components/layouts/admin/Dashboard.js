aec0216b0e990598d63e96fb4f9153d59d1b5ec8
Lecoeurdelest <=>
2023-07-13 01:34:29 +0700

    quy

---
+++ RentFinder/src/components/layouts/admin/Dashboard.js
@@ -0,0 +1,19 @@
+import React from "react";
+// react-bootstrap components
+import { Badge, Button, Card, Navbar, Nav, Table, Container, Row, Col, Form, OverlayTrigger, Tooltip } from "react-bootstrap";
+
+function Dashboard() {
+    return (
+        <>
+            <Container fluid>
+                <Row>
+                    <Col md="12">
+                        <h1>WELCOME TO MY DASHBOARD</h1>  
+                    </Col>
+                </Row>              
+            </Container>
+        </>
+    );
+}
+
+export default Dashboard;
